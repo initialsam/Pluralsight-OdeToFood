cc1b013873a63f6f8c940b63a2a311475d228d91
sam <evaoosyz@gmail.com>
2019-12-09 16:47:10 +0800

    加入專案檔案。

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMomoryRestaurantData : IRestaurantData
+    {
+        public List<Restaurant> Restaurants { get; }
+        public InMomoryRestaurantData()
+        {
+            Restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id=1, Name="Pizzea",Location="aaaaa", Cuisine=CuisineType.Italian },
+                new Restaurant { Id=2, Name="Cinnamon",Location="bbbbb", Cuisine=CuisineType.Indian },
+                new Restaurant { Id=3, Name="Costa",Location="cccccc", Cuisine=CuisineType.Mexican }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in Restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        public string Message { get; set; }
+        public string AppSettingMessage { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config,
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            this.Message = "Hello";
+            this.AppSettingMessage = this.config["Message"];
+            this.Restaurants = this.restaurantData.GetAll();
+        }
+    }
+}
