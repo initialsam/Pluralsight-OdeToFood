dee492b808d682654808e7bd56d9887c2a0d9ecd
sam <evaoosyz@gmail.com>
2019-12-24 16:21:41 +0800

    Integrating Client-side JavaScript and CSS Done

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,6 +31,7 @@ namespace OdeToFood
             });
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +50,7 @@ namespace OdeToFood
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseNodeModules();
             app.UseRouting();
 
             app.UseAuthorization();
@@ -57,6 +58,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
